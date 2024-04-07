91e84765add3605224e617ea24d7ff57f70eeaeb
JoaquimT6 <jotaa45@outlook.com>
2024-04-07 18:48:26 -0300

    Create Task.java

---
+++ Código/src/main/java/com/labdessoft/roteiro01/entity/Task.java
@@ -0,0 +1,71 @@
+package com.labdessoft.roteiro01.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Task {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime dueDate;
+    private boolean completed;
+
+    // Constructors
+    public Task() {
+    }
+
+    public Task(String title, String description, LocalDateTime dueDate, boolean completed) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
