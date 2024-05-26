0b2380a1da2c74b55c2f76eca6963b9cf2c62fda
JoaquimT6 <jotaa45@outlook.com>
2024-05-26 18:16:20 -0300

    Atualização

--- Codigo/src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
+++ Codigo/src/main/java/com/labdessoft/roteiro01/Roteiro01Application.java
@@ -2,8 +2,10 @@ package com.labdessoft.roteiro01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.labdessoft.roteiro01"})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.labdessoft.roteiro01.repository")
 public class Roteiro01Application {
 
 	public static void main(String[] args) {
